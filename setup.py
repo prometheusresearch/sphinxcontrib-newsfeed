104741d51ac035f4d3d8c513e4490d016b8f7c68
Kirill Simonov <xi@resolvent.net>
2015-02-18 17:53:16 -0600

    Bumped the version number.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 
 NAME = "sphinxcontrib-newsfeed"
-VERSION = "0.1.3"
+VERSION = "0.1.4"
 DESCRIPTION = "News Feed extension for Sphinx"
 LONG_DESCRIPTION = open('README').read()
 AUTHOR = "Kirill Simonov (Prometheus Research, LLC)"
